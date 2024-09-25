2a655411ddcaf7cbec34f84fc66e06142e366f80
axelnoguera <axelnoguera667@gmail.com>
2024-09-25 15:45:09 -0300

    primer commit

---
+++ producto3/script.js
@@ -0,0 +1,24 @@
+let imgs = document.querySelectorAll(".img-mini");
+let imgUno = document.querySelector("#imgUno");
+let imgDos = document.querySelector("#imgDos");
+let imgTres = document.querySelector("#imgTres");
+
+
+let contenedores = [imgUno,imgDos,imgTres];
+let src = ["","",""];
+
+
+
+for(let i = 0;i < 3;i++){
+  contenedores[i].src = src[i];
+};
+
+
+
+imgs.forEach(imgs => {
+    imgs.addEventListener("click", () => {
+        const srcPrincipal = imgUno.src;
+        imgUno.src = imgs.src;
+        imgs.src = srcPrincipal;
+    });
+});
